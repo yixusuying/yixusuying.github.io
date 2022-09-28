7910c89419682b56f6acaf82bc8d48e002f6ff49
yixusuying <735490965@qq.com>
2022-09-28 11:26:51 +0800

    Site updated: 2022-09-28 11:26:50

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@
 					.appendTo($body)
 					.panel({
 						delay: 500,
-						hideOnClick: true,
+						hideOnClick: false,
 						hideOnSwipe: true,
 						resetScroll: true,
 						resetForms: true,
